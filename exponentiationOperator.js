99d27c915af27a97a34d0d646535944cea29bc21
Muhammad Farooq <irfankhans786787788@gmail.com>
2024-12-01 20:35:09 +0500

    exponential operator

---
+++ exponentiationOperator.js
@@ -0,0 +1,6 @@
+// The Exponentiation Operator (**) is used to raise a number to a power. It is a shorthand for Math.pow() and provides a cleaner syntax.
+console.log(2 ** 3); // 8 (2 raised to the power of 3)
+console.log(5 ** 2); // 25 (5 squared)
+
+console.log(Math.pow(2, 3)); // 8
+console.log(2 ** 3); // 8
