41dd79afea8f5fc10cde18b7adf47c8d22ffc7e5
Muhammad Farooq <irfankhans786787788@gmail.com>
2024-12-01 21:58:59 +0500

    string methods

---
+++ stringMethods.js
@@ -0,0 +1,47 @@
+// const str = "Hello World!";
+// console.log(str.length); // Output: 12
+
+// console.log("JavaScript".charAt(4)); // Output: S
+
+// console.log(str.charCodeAt(0)); // Output: 65
+
+// console.log("hello".toUpperCase()); // Output: HELLO
+// console.log("WORLD".toLowerCase()); // Output: world
+
+// console.log("  Hello World  ".trim()); // Output: "Hello World"
+
+// const str = "Muhammad Farooq";
+// console.log(str.slice(-6));
+
+// console.log("JavaScript".substring(4, 10)); // Output: Script
+
+// console.log("I love JavaScript".replace("love", "like"));
+// Output: I like JavaScript
+
+// console.log("apple apple apple".replaceAll("apple", "orange"));
+// Output: orange orange orange
+
+// console.log("Hello World".indexOf("World")); // Output: 6
+
+// console.log("Hello World World".lastIndexOf("World")); // Output: 12
+
+// console.log("JavaScript".includes("Script")); // Output: true
+
+// console.log("JavaScript".startsWith("Java")); // Output: true
+
+// console.log("JavaScript".endsWith("Script")); // Output: true
+
+// console.log("a,b,c".split(",")); // Output: ['a', 'b', 'c']
+
+// console.log("Hello".concat(" ", "World!")); // Output: Hello World!
+
+// console.log("Ha".repeat(3)); // Output: HaHaHa
+
+// console.log("5".padStart(3, "0")); // Output: 005
+
+// console.log("5".padEnd(3, "0"));   // Output: 500
+
+// console.log("abc123".match(/\d+/)); // Output: ['123']
+
+// const num = 123;
+// console.log(num.toString()); // Output: "123"
